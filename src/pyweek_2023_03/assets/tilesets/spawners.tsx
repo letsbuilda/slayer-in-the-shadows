50dfe7c4d2033a4843cf6bf04efb89cfc2ae719c
Stickie <usethecorrectformat@gmail.com>
2023-03-31 14:41:40 +1100

    fix brick sizing

--- src/pyweek_2023_03/assets/tilesets/spawners.tsx
+++ src/pyweek_2023_03/assets/tilesets/spawners.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="spawners" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="spawners" tilewidth="16" tileheight="16" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="64" height="64" source="../sprites/spawners/player_spawner.png"/>
+  <image width="16" height="16" source="../sprites/spawners/player_spawner.png"/>
  </tile>
  <tile id="1">
-  <image width="64" height="64" source="../sprites/spawners/enemy_spawner.png"/>
+  <image width="16" height="16" source="../sprites/spawners/enemy_spawner.png"/>
  </tile>
 </tileset>
